a46c20f251aceff9d63f1e6d15477b8df0e959f1
Pratik Patil <pratik7368patil@gmail.com>
2022-06-02 10:53:49 +0530

    Typography issue fixed and responsive font's added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Typography from '@mui/material/Typography';
 
 function App() {
-  return (
-    <div className="App">
-      Clean React App
-    </div>
-  );
+    const comps = ["h1", "h2", "h3", "h4", "h5", "h6", "body1", "body2", "subtitle1", "subtitle2"];
+    return (
+        <div className="App">
+            {comps.map((variant, i) => {
+                return <Typography variant={variant} key={i}>Clean React App</Typography>
+            })}
+        </div>
+    );
 }
 
 export default App;
